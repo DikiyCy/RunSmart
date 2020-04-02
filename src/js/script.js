4f43a34e3f5ac8a0789ad48f1ce381ef5e6be277
Mike <9834088@gmail.com>
2020-04-02 22:49:49 +0300

    слайдер несколько способов

---
+++ src/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/s4_carousel/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/s4_carousel/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+      });
+  });
+
+
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+//   });
+
+//   document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+//   });
+//   document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+//   });
